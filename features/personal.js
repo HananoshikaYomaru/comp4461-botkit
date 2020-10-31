f9db58af49d1ad42c5ad011a5fdb5ed42e4f6923
Yomaru <mlkyeung@connect.ust.hk>
2020-10-31 12:12:30 +0800

    add personal

---
+++ features/personal.js
@@ -0,0 +1,43 @@
+const { BotkitConversation } = require('botkit');
+const {zip , assign} = require("../utils/zip") ; 
+const {andTest} = require("../utils/condition")
+
+const set1 = ["how are you" , "are you fine"]
+const set2 = ["hungry" , "starving" , "food"]
+const set3 = ["you" , "olaf"]
+const set4 = ["favourite" , "like" , "enjoy" , "love"]
+const set5 = ["weather" , "season"]
+const set6 = ["name", "call"]
+
+module.exports = function (controller ) {
+    var result = {} ;
+
+    const trigger = (text) => {
+        result = {
+            success1 :  andTest(text, [ set1 ]) , 
+            success2 :  andTest(text, [set2, set3]) , 
+            success3 : andTest(text,  [set3, set4, set5]), 
+            success4 : andTest(text, [set3, set6])
+        }
+           
+        return result; 
+    } ; 
+
+    controller.hears (async (message) => trigger(message.text) && result.success1 , 'message,direct_message,direct_mention', async(bot, message) => {
+        await bot.reply(message , "Oh...😚 you are so lovely, I am fine") ; 
+    });
+
+    controller.hears (async (message) => trigger(message.text) && result.success2 , 'message,direct_message,direct_mention', async(bot, message) => {
+        await bot.reply(message , "I am not hungry, but I like ice cream 🤤") ; 
+    });
+
+    controller.hears (async (message) => trigger(message.text) && result.success3 , 'message,direct_message,direct_mention', async(bot, message) => {
+        await bot.reply(message , "put me in summer and I'll be a — happy snowman! 🥴") ; 
+    });
+
+    controller.hears (async (message) => trigger(message.text) && result.success4 , 'message,direct_message,direct_mention', async(bot, message) => {
+        await bot.reply(message , "I am Olaf, the only snowman ⛄ in Summer ⛱") ; 
+    });
+
+    
+}
